a9470da998e40b7a395641b5d833e731c26cd4f9
Rafał Jadwiżak <rafu352@gmail.com>
2024-04-01 12:40:25 +0200

    Dodaj pliki projektów.

---
+++ ZadDomowe/Program.cs
@@ -0,0 +1,17 @@
+﻿/// zadanie domowe
+
+string name = "Ewa";
+string sex = "kobieta";
+var age = 30;
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat.");
+}
+else if (sex == "kobieta" && name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30.");
+}
+else if (sex != "kobieta" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna.");
+}
